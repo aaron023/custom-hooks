6e9beb0bf79f9f60a1acd18928153a203b703387
Aaron Carrizales <aaronacr@gmail.com>
2023-09-13 17:26:25 -0600

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,51 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from "../08.-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [] ;
+}
+
+export const useTodos = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, [], init);  
+
+    // const todosCount = todos.length;
+    // const pendingTodosCount = todos.filter(todo => !todo.done).length;
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+    
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToogleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toogle Todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleDeleteTodo,
+        handleToogleTodo,
+        handleNewTodo
+    }
+}
